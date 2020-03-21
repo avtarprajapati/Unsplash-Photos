b1f7e5079212a7f48767775b48e141c7e5627bcc
Avtar Prajapati <prajaptiavtar@gmail.com>
2020-03-21 20:39:58 +0530

    add dist folder

---
+++ build/precache-manifest.b198413ef6871f26b518fa69cbde3320.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02616479e31ef33f22fb7e6e57ed7e35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be2ca5de0b45296e1ec3",
+    "url": "/static/css/main.84a9c081.chunk.css"
+  },
+  {
+    "revision": "23f77ea86c378fdf7dfb",
+    "url": "/static/js/2.88fd7d07.chunk.js"
+  },
+  {
+    "revision": "970d7393064dc8df8708ab396e4ac92c",
+    "url": "/static/js/2.88fd7d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2ca5de0b45296e1ec3",
+    "url": "/static/js/main.1b312380.chunk.js"
+  },
+  {
+    "revision": "12da6d54b81111279a24",
+    "url": "/static/js/runtime-main.a80a271f.js"
+  }
+]);
